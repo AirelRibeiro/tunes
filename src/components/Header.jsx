c3c7c7773bd65d561aa4440e4fe920c99265fd1c
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-04-18 19:18:48 -0300

    Adução de tag de navegação com Links router, para atender ao requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Carregando from '../Carregando';
 
@@ -29,7 +30,14 @@ class Header extends React.Component {
     const { name, carregando } = this.state;
     return (
       <header data-testid="header-component">
+        <nav>
+          <Link data-testid="link-to-search" to="/search">Search</Link>
+          <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+          <Link data-testid="link-to-profile" to="/profile">Profile</Link>
+        </nav>
+
         {carregando ? <Carregando /> : <h1 data-testid="header-user-name">{ name }</h1>}
+
       </header>
     );
   }
