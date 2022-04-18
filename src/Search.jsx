1e227d4dc2657c85fa7463cf1b011deaad38fe71
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-04-18 19:55:35 -0300

    Adição de lógica do requisito 5 no arquivo Search.jsx

--- src/Search.jsx
+++ src/Search.jsx
@@ -2,10 +2,37 @@ import React from 'react';
 import Header from './components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      input: value,
+    });
+  }
+
   render() {
+    const { input } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <input
+          data-testid="search-artist-input"
+          value={ input }
+          onChange={ this.handleChange }
+        />
+        <button
+          data-testid="search-artist-button"
+          type="button"
+          disabled={ input.length < 2 }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
